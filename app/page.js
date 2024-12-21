f2af47c284c1a3ca6373df2f03e65407b7a7ca58
DiegoJohnsonL <diegojohnson46@gmail.com>
2024-12-21 14:07:10 -0300

    disable ssr

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
-import About from "./components/about";
 const Hero = dynamic(() => import("./components/hero"), { ssr: false });
+const About = dynamic(() => import("./components/about"), { ssr: false });
+
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
